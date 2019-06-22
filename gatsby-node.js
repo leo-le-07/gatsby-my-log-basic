7d71a1f4ebb207bcf60bbb9b3f9cadfaf0e7fc58
Leo <coder.leo.le@gmail.com>
2019-06-22 09:27:14 +0700

    Fix error deploy with gatsby-feed

--- gatsby-node.js
+++ gatsby-node.js
@@ -45,15 +45,16 @@ exports.createPages = ({ graphql, actions }) => {
   })
 }
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+// exports.onCreateNode = ({ node, actions, getNode }) => {
+//   const { createNodeField } = actions
 
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
-}
+//   console.log('==== node.internal', node.internal);
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const value = createFilePath({ node, getNode })
+//     createNodeField({
+//       name: `slug`,
+//       node,
+//       value,
+//     })
+//   }
+// }
