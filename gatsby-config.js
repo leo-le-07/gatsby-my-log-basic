79493a2152115a941ed5888801d3a3d1f8635db4
Leo <coder.leo.le@gmail.com>
2019-06-23 09:24:15 +0700

    Update logo

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,13 +79,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Trader Viet Coin`,
+        short_name: `TraderVietCoin`,
         start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
+        background_color: `#222`,
+        theme_color: `#F7D02D`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/icon.png`,
       },
     },
     `gatsby-plugin-offline`,
